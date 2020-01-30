f170d8612094b29ce1945b8186457f6f1b927107
Kadievka <kadievka.s.ks@gmail.com>
2020-01-30 10:50:39 -0400

    funciona

--- html/server.js
+++ html/server.js
@@ -2,14 +2,12 @@ const express = require ('express'); //importando la librería de express
 
 const app = express(); // objeto app con el que definimos rutas
 
-app.use('/assets', express.static('assets'));// el metodo use del objeto app inserta un nuevo middleware
+app.set('view engine', 'ejs');// hay que hacerlo para que sepa que es un motor de vistas lo que vamos a enviar y cuál es
+
+app.use('/assets', express.static('assets'));
 
 app.get('/', function (req, resp) {
-    resp.sendFile('index.html',
-        {
-            root: __dirname
-        });
-
+    resp.render('index')//método para enviar una vista después que se configura el motor de vistas
 });
 
 app.listen(3000);// ponerlo en el puerto 3000
