b80265db9854f4c22deeeb6a2b1e7c511e5b3f42
Kadievka <kadievka.s.ks@gmail.com>
2020-01-30 09:54:39 -0400

    configurdo y una ruta post implementada

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require ('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extented: true}));
+// le estamos pidiendo a express use bodyparser para analizar el cuerpo de datos de la petición
+
 app.get('/saludo', function (req, res) {
     res.send(`Hola ${req.query.name}`);
+});
+
+app.post('/', function (req, res) {
+    res.send(`Hola ${req.body.name}`);
 });
 
 app.listen(3000);
