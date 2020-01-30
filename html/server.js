9ffdc8cdc50a3675685fe63860aaac03c1b592d5
Kadievka <kadievka.s.ks@gmail.com>
2020-01-30 10:19:34 -0400

    ahora si escribo http://localhost:3000/style.css podré ver que el archivo de assets se está importando

--- html/server.js
+++ html/server.js
@@ -2,13 +2,13 @@ const express = require ('express'); //importando la librería de express
 
 const app = express(); // objeto app con el que definimos rutas
 
-app.get('/', function (req, resp) { //la ruta get que recibe como parametros la ruta y la funcion manejadora, que recibe a su vez
-    //la petición y la respuesta
-    //resp.send(__dirname); //__dirname es una variable que devuelve la ruta absoluta del proyecto
+app.use(express.static('assets'));// el metodo use del objeto app inserta un nuevo middleware
+
+app.get('/', function (req, resp) {
     resp.sendFile('index.html',
         {
             root: __dirname
-        });// método sendFile (path, options, callback) que tiene el objeto resp
+        });
 
 });
 
