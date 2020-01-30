b33911fcc6caf2c85767e79a4da762aac6244c58
Kadievka <kadievka.s.ks@gmail.com>
2020-01-30 14:07:57 -0400

    comienzo base de datos

--- cookies-sesiones/server.js
+++ cookies-sesiones/server.js
@@ -1,16 +1,18 @@
 const express = require ('express');
+const cookieSession = require ('cookie-session');// puede encriptar y desencriptar cookies
 
 const app = express();
 
-app.set('view engine', 'ejs');
-
-app.use('/assets', express.static('assets',{
-    etag: false,
-    maxAge: '5h'// valor máximo es 1 año para que el caché expire
+app.use(cookieSession({//atributos de configuración
+    name: 'session', //es una vlor por defecto
+    keys: ['Kadievka','Salcedo','25','kadievka@gmail.com']//arreglo de datos para verificación
 }));
 
-app.get('/', function (req, resp) {
-    resp.render('index')
+app.get('/', function(req, resp){
+    req.session.visits = req.session.visits || 0;
+    req.session.visits = req.session.visits + 1;
+
+    resp.send(`${req.session.visits} visitas`);
 });
 
 app.listen(3000);
