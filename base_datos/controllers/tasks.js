3e69452bec0640fe5180369aa01c49c0d8f4e9a6
Kadievka <kadievka.s.ks@gmail.com>
2020-01-31 09:14:38 -0400

    usar view-engine pug

---
+++ base_datos/controllers/tasks.js
@@ -0,0 +1,8 @@
+const Task = require('../models').Task //así importamos todos modelos y específicamente el modelo Task
+
+module.exports = {
+    home: function(){// claves del nombre con las que se identificarán las funciones
+                     //estas funciones son manejadoras d epeticiones http
+
+    }
+}
--- base_datos/server.js
+++ base_datos/server.js
@@ -7,24 +7,13 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-const sequelize = new Sequelize('proyecto-backend', null, null,{
-    dialect: 'sqlite',
-    storage: 'proyecto-backend'
-});
+//Sequelize se va a conectar por su cuenta//
 
-let db = new sqlite3.Database('proyecto-backend');
+app.use('view-engine', 'pug');
 
 app.post('/pendientes', function(req, resp){
     //db.run(`INSERT INTO tasks (description) VALUES (?)`, req.body.description);
     resp.send('Inserción finalizada');
 });
 
-
-
 app.listen(3000);
-
-process.on('SIGINT', function(){//este objeto nos permite escuchar eventos del proceso
-    console.log('Adiós - Atte. El Servidor');
-    db.close();//cerrar la conexion
-    process.exit();//termina el servidor de node
-});
