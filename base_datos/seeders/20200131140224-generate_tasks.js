eb757cc91a0465ba4d5eaacf71206602df1908ac
Kadievka <kadievka.s.ks@gmail.com>
2020-01-31 10:14:59 -0400

    un seeder que funciona

---
+++ base_datos/seeders/20200131140224-generate_tasks.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+      return queryInterface.bulkInsert('tasks', [
+        {id: 1, description: 'Grabar el video curso de Backend', createdAt:new Date(), updatedAt: new Date()},
+        {id: 2, description: 'Editar los videos del curso de Backend', createdAt:new Date(), updatedAt: new Date()},
+        {id: 3, description: 'Subir los videos', createdAt:new Date(), updatedAt: new Date()}
+      ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+      return queryInterface.bulkDelete('Task', null, {});
+  }
+};
