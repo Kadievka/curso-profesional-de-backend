1aa7f97c480e61f5e15f831ded241c27572263bb
Kadievka <kadievka.s.ks@gmail.com>
2020-01-31 08:15:08 -0400

    instalado sequelize globalmente e iniciado

--- base_datos/server.js
+++ base_datos/server.js
@@ -1,17 +1,21 @@
 const express= require('express');
 const sqlite3 = require('sqlite3');
 const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+const sequelize = new Sequelize('proyecto-backend', null, null,{
+    dialect: 'sqlite',
+    storage: 'proyecto-backend'
+});
+
 let db = new sqlite3.Database('proyecto-backend');
 
-//db.run('CREATE TABLE tasks(id int AUTO_INCREMENT, description varchar(255))');//correr una consulta SQL
-
 app.post('/pendientes', function(req, resp){
-    db.run(`INSERT INTO tasks (description) VALUES (?)`, req.body.description);
+    //db.run(`INSERT INTO tasks (description) VALUES (?)`, req.body.description);
     resp.send('Inserción finalizada');
 });
 
