2d09cce3d53436c82fec0378fccbd42366a64750
Kadievka <kadievka.s.ks@gmail.com>
2020-02-03 14:01:11 -0400

    hay un error

--- base_datos/controllers/sessions.js
+++ base_datos/controllers/sessions.js
@@ -1,8 +1,15 @@
+const User = require('../models').User; //importamos el modelo user.js
+
 module.exports = {//linea típica para iniciar un controlador
     new: function(req, res){//linea típica para iniciar un controlador despliega el formulario
         res.render('sessions/new.pug');//enviar una vista
     }, 
     create: function(req, res){//linea típica para iniciar un controlador despliega el formulario
-
+        User.login(req.body.email, req.body.password)
+                .then(user=> res.json(user))
+                .catch(err=>{
+                    res.json(err);
+                    console.log(res);
+                });
     }, 
 };
