404ce33065bd48ac1e44dd6dae5c52c41dd65780
Kadievka <kadievka.s.ks@gmail.com>
2020-02-03 11:09:59 -0400

    utilicé bcrypt-node.js y esperemos que funcione

--- base_datos/models/user.js
+++ base_datos/models/user.js
@@ -1,11 +1,23 @@
 'use strict';
+
+const bcrypt = require('bcrypt-nodejs');
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    email: DataTypes.STRING,
+    email: { // es más avanzado pasar parametros en un JSON con más configuraciones
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    }
     password_hash: DataTypes.STRING
   }, {});
   User.associate = function(models) {
     // associations can be defined here
   };
+  User.beforeCreate(function(user, options){// para encritar el password hacer un hook
+    bcrypt.hash(password, 10, function(){
+      
+    })
+  });
   return User;
 };
