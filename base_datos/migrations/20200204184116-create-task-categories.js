29bfb856b5cf6afee90bb00973ab3ca028b1ddbd
Kadievka <kadievka.s.ks@gmail.com>
2020-02-04 14:41:38 -0400

    model y migracion de TaskCategories

---
+++ base_datos/migrations/20200204184116-create-task-categories.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('TaskCategories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      taskId: {
+        type: Sequelize.INTEGER
+      },
+      categoryId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('TaskCategories');
+  }
+};
