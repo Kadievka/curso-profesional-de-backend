9d7e7970ca22ee87cf58d8877415e2720b46b6ea
Kadievka <kadievka.s.ks@gmail.com>
2020-02-04 11:02:28 -0400

    limpiando comentarios

--- base_datos/controllers/sessions.js
+++ base_datos/controllers/sessions.js
@@ -1,16 +1,14 @@
-const User = require('../models').User; //importamos el modelo user.js
+const User = require('../models').User;
 
-module.exports = {//linea típica para iniciar un controlador
-    new: function(req, res){//linea típica para iniciar un controlador despliega el formulario
-        res.render('sessions/new.pug');//enviar una vista
+module.exports = {
+    new: function(req, res){
+        res.render('sessions/new.pug');
     }, 
-    create: function(req, res){//linea típica para iniciar un controlador envia los datos del formulario
-        //console.log('Estoy pasando por el controlador de sesiones');
+    create: function(req, res){
         User.login(req.body.email, req.body.password)
             .then(user=> {
                 if(user){
                     req.session.userId = user.id
-                    console.log('la sesion tuvo exito y tengo el id del usuario: ' + req.session.userId);
                     res.json(user);
                 }
             })
--- base_datos/middlewares/find_user.js
+++ base_datos/middlewares/find_user.js
@@ -2,15 +2,11 @@ const User = require('../models').User;
 
 module.exports = function(req, res, next){
 
-    //console.log('holis req.session.userId = ' + req);
-
     if( !req.session.userId){
-        console.log('no obtuve req.session.userId en el middleware');
         return next();
     } 
 
     User.findByPk(req.session.userId).then(user=>{
-        console.log('si obtuve req.session.userId en el middleware');
         if(user){
             req.user = user;
             next();
